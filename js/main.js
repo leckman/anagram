51151e055982e4685b048db852e823c8c3b903dd
Laura Eckman <eckmanlw@gmail.com>
2016-10-14 16:15:21 -0400

    anagram checker v1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$().ready(function(){
+
+	var checker = $("#checker");
+
+	var reverse = function(word) {
+		return word.split("").reverse().join("");
+	}
+
+	var anagram = function(word1, word2) {
+		// TODO actual implementation
+		return word1 == reverse(word2);
+	};
+
+	$("input").change(function()
+	{
+		// verify anagram
+		var left = $("#input1").val();
+		var right = $("#input2").val();
+
+		if (left == "" || right == "") {
+			// not yet entered
+			checker.attr("class", "centered waiting");
+		} else if (anagram(left, right)) {
+			checker.attr("class", "centered valid");
+		} else {
+			checker.attr("class", "centered invalid");
+		}
+	});
+});
